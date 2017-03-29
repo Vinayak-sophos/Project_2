56cc1cb959916b3020229b198100c26a93a505fd
Salil Jain <j.salil36@gmail.com>
2017-03-29 10:50:11 +0000

    Click image to enlarge added

--- View/store.php
+++ View/store.php
@@ -1,3 +1,5 @@
+<body>
+    <link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style2.css">
 <h1>Store</h1>
 <form method="post" action="<?php echo($_SEVRER['PHP_SELF']); ?>">
     <input type="text" name="search" placeholder="Search" />
@@ -17,8 +19,23 @@ else {
     <tr><th>Image</th><th>Title</th><th>Price</th><th>College</th><th>Category</th><th>Date</th><th>Contact Seller</th></tr>
     <?php for($i = 0; $i < $len; $i++){
         $name = "num_".$i;
-        echo("<tr><td><img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='70px' height='70px' /></td><td>{${$name}['item_name']}</td>
-        <td>{${$name}['price']}</td><td>{${$name}['college']}</td><td>{${$name}['category']}</td><td>{${$name}['date']}</td>
-        <td><a href='".generate_url('store', 'contact_seller')."&item_id={${$name}['item_id']}'>Contact Seller</a></td></tr>");
+        echo("<tr>
+                <td>
+                    <a class=\"lightbox\" href=\"#full\">
+                        <img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='70px' height='70px' />
+                    </a>
+                    <div class=\"lightbox-target\" id=\"full\">
+                        <img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' />
+                    <a class=\"lightbox-close\" href=\"#\"></a>
+                    </div>
+                </td>
+                <td>{${$name}['item_name']}</td>
+                <td>{${$name}['price']}</td>
+                <td>{${$name}['college']}</td>
+                <td>{${$name}['category']}</td>
+                <td>{${$name}['date']}</td>
+                <td><a href='".generate_url('store', 'contact_seller')."&item_id={${$name}['item_id']}'>Contact Seller</a></td>
+            </tr>");
     } ?>
 </table>
+</body>
