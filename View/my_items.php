9ee2b122ca8c51bf20948aadc702063199c49a71
Salil Jain <j.salil36@gmail.com>
2017-04-01 11:23:37 +0000

    Frontend for home

--- View/my_items.php
+++ View/my_items.php
@@ -1,17 +1,38 @@
-<h3><?php if (!$len) {
-    echo("You have not put any item on sale, <a href='".generate_url("store", "add_items")."'>sell now</a>");
-    exit;
-} else if ($len == 1) {
-    echo("You put 1 item on sale");
-}
-else {
-    echo("You put {$len} items on sale");
-} ?></h3>
-<table>
-    <tr><th>Image</th><th>Title</th><th>Description</th><th>Price</th><th>Date</th><th>Remove</th></tr>
-    <?php for($i = 0; $i < $len; $i++){
-        $name = "num_".$i;
-        echo("<tr><td><img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='70px' height='70px'/></td><td>{${$name}['item_name']}</td><td>{${$name}['description']}</td><td>{${$name}['price']}</td><td>{${$name}['date']}</td>
-        <td><button onclick=location.href='".generate_url("store", "remove")."&item_id={${$name}['item_id']}'>Remove</button></td></tr>");
-    } ?>
-</table>
+<body>
+    <link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style3.css">
+    <h3><?php if (!$len) {
+        echo("You have not put any item on sale, <a href='".generate_url("store", "add_items")."'>sell now</a>");
+        exit;
+    } 
+    else if ($len == 1) {
+        echo("<div id = \"numitems\">You put 1 item on sale</div>");
+    }
+    else {
+        echo("<div id = \"numitems\">You put {$len} items on sale</div>");
+    } ?></h3>
+        
+        <?php for($i = 0; $i < $len; $i++){
+            $name = "num_".$i;
+            echo("
+                <div id = \"item\">
+                    <img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='70px' height='70px'/>
+                    
+                    <div class=\"desc-div\">
+                        <div id = \"name\">
+                            {${$name}['item_name']}
+                        </div>
+                        <div id = \"desc\">
+                            {${$name}['description']}
+                        </div>
+                        <div id = \"date\">
+                            Uploaded for selling on {${$name}['date']}
+                        </div>
+                    </div>
+                </div>
+            `       <button class = \"buttn\" \"button\" onclick=location.href='".generate_url("store", "remove")."&item_id={${$name}['item_id']}'>Remove</button>
+                <div id = \"line\"><hr></div>
+                ");
+            
+        } ?>
+
+</body>
