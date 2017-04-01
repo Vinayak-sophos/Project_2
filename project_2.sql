450857850d568e2503f63121eec8d1982f1df2a3
Vinayak Sachdeva <vishusachdeva228@gmail.com>
2017-04-01 19:58:14 +0000

    project_2.sql

--- project_2.sql
+++ project_2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2017 at 07:06 AM
+-- Generation Time: Apr 01, 2017 at 07:55 PM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.20
 
@@ -19,6 +19,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `project_2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE IF NOT EXISTS `cart` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `item_name` varchar(200) NOT NULL,
+  `price` int(11) NOT NULL,
+  `category` varchar(200) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `item_id` (`item_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -54,16 +72,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `balance` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `college`, `email`, `gender`, `password`) VALUES
-(1, 'Vinayak Sachdeva', 'c3', 'vishusachdeva228@gmail.com', 'Male', '$2y$10$ciuOqYzI3YNs9GFVSdtCneWnbBUuxBVOymRJ3to2BoykohJ2Gmkui'),
-(3, 'Vishu', 'c2', '2015ucp1057@mnit.ac.in', 'Male', '$2y$10$OOdht.OcWfYTWCFWLPtRpOvtJn.VoEooXEWDmKQgjLPzRKNv/O51e');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
