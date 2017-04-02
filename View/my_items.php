dd1a522305a6bff9ff0ce373c217238fb2da5059
Salil Jain <j.salil36@gmail.com>
2017-04-02 16:59:03 +0000

    updated my_items.php

--- View/my_items.php
+++ View/my_items.php
@@ -1,14 +1,15 @@
 <body>
     <link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style3.css">
+    <div class = "notnavigation">
     <h3><?php if (!$len) {
         echo("You have not put any item on sale, <a href='".generate_url("store", "add_items")."'>sell now</a>");
         exit;
     } 
     else if ($len == 1) {
-        echo("<div id = \"numitems\">You put 1 item on sale</div>");
+        echo("<div id = \"numitems\">You have put 1 item on sale</div>");
     }
     else {
-        echo("<div id = \"numitems\">You put {$len} items on sale</div>");
+        echo("<div id = \"numitems\">You have put {$len} items on sale</div>");
     } ?></h3>
         
         <?php for($i = 0; $i < $len; $i++){
@@ -29,10 +30,10 @@
                         </div>
                     </div>
                 </div>
-            `       <button class = \"buttn\" \"button\" onclick=location.href='".generate_url("store", "remove")."&item_id={${$name}['item_id']}'>Remove</button>
+                   <button class = \"buttn\" \"button\" onclick=location.href='".generate_url("store", "remove")."&item_id={${$name}['item_id']}'>Remove</button>
                 <div id = \"line\"><hr></div>
                 ");
             
         } ?>
-
+    </div>
 </body>
