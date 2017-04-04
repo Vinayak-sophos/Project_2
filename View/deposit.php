49f2898a306f5c8c01016f109a2b4f2977f0560f
Salil Jain <j.salil36@gmail.com>
2017-04-04 10:29:00 +0000

    deposit option done

--- View/deposit.php
+++ View/deposit.php
@@ -1,4 +1,7 @@
-<form action="<?php echo(generate_url('store', 'deposit')); ?>" method="post">
-    <input type="number" name="deposit" />
-    <button tpe="submit"> Deposit Money</button>
-</form>
+<link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style6.css">
+<div id = "depositform">
+    <form action="<?php echo(generate_url('store', 'deposit')); ?>" method="post">
+        <div id = "enteramount">Amount(Rs.)</div><input type="number" name="deposit"/>
+        <button type="submit"> Deposit Money</button>
+    </form>
+</div>
