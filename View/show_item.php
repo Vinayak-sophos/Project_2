cfa5370bff8fb0ab9c192129c9ce3f7ceff2ba74
Salil Jain <j.salil36@gmail.com>
2017-04-06 13:18:28 +0000

    More info - frontend done

--- View/show_item.php
+++ View/show_item.php
@@ -1,14 +1,36 @@
+<link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style7.css">
 <?php
     $name = "num_0";
-    echo("<img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='70px' height='70px' /><br/>
-    {${$name}['item_name']}<br/>
-    {${$name}['description']}<br/>
-    {${$name}['contact']}<br/>
-    {${$name}['choice']}<br/>
-    {${$name}['price']}<br/>
-    {${$name}['college']}<br/>
-    {${$name}['category']}<br/>
-    {${$name}['date']}<br/>
-    <a href='".generate_url('store', 'contact_seller')."&item_id={${$name}['item_id']}'>Contact Seller</a><br/>
-    <a href='".generate_url('store', 'buy')."&item_id=".${$name}['item_id']."'>Add this item to cart</a>");
+    echo("<img src='".UPLOAD_PATH.${$name}['image']."' alt='".${$name}['image']."' width='400px' /><br/>
+    <div class = \"info\">
+        <div class = \"itemname\">
+            <b>{${$name}['item_name']}</b><br/>
+        </div>
+        <div class = \"category\">    
+            ({${$name}['category']})<br/>
+        </div>
+        <div class = \"desc\">
+            {${$name}['description']}<br/>
+        </div>");
+        if (${$name}['choice']== "donate")
+        {
+            echo("<div class = \"sell\">Seller is donating this item.</div>");
+        }
+        else
+        {
+            echo("<div class = \"sell\">Selling at Rs. ".${$name}['price']."</div>");
+        }
+    echo("        
+        <div class = \"college\">    
+            College - {${$name}['college']}<br/>
+        </div>
+        <div class = \"date\">    
+            Uploaded for sale on {${$name}['date']}<br/>
+        </div>
+        <a href='".generate_url('store', 'contact_seller')."&item_id={${$name}['item_id']}'><img class = \"contactseller\" src = \"../img/contactseller.png\" width = \"80px\"></a><br/>
+        <a href='".generate_url('sctore', 'buy')."&item_id=".${$name}['item_id']."'><img class = \"addtocart\" src = \"../img/addtocart.png\" width = \"80px\"></a>
+        <div id = \"contacttext\">Contact Seller</div>
+        <div id = \"addtext\">Add to Cart</div>
+    </div>"
+        );
 ?>
