818e6fb0270153e2ab88da051bd7103dced16766
Salil Jain <j.salil36@gmail.com>
2017-04-06 16:48:37 +0000

    modified frontend of home

--- View/home.php
+++ View/home.php
@@ -1,4 +1,18 @@
 <link rel="stylesheet" href="<?php echo(CSS_PATH); ?>style5.css">
+<div id = "storeicon">
+    <a href="<?php echo(generate_url('store', 'store')); ?>">
+    <img src = "../img/buyitem.png" class = "homeicon">
+    <div id = "storetext">
+        <p>Buy</p></a></br>
+    </div>
+</div>
+<div id = "sellicon">
+    <a href="<?php echo(generate_url('store', 'add_items')); ?>">
+    <img src = "../img/sellitem.png" class = "homeicon">
+    <div id = "selltext">
+        <p>Sell</p></a><br/>
+    </div>
+</div>
 <div id = "balance">
     <img src = "../img/balance.png" class = "homeicon">
     <div id = "balancetext">
