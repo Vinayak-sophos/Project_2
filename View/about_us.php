7712fe18f06b7e5819e2a536adf9d71bb9133428
Salil Jain <j.salil36@gmail.com>
2017-04-17 17:29:52 +0000

    Final changes

--- View/about_us.php
+++ View/about_us.php
@@ -1,30 +1,101 @@
-<br/><br/><br/><br/>
-<b>Sophomores.in<br/>
-Project_2<br/>
-DilSeDeal</b><br/>
-<br/>
-<b>Front End Developer</b><br/>
-<br/>
-Salil<br/>
-<br/>
-College - <br/>
-<br/>
-Email ID - <br/>
-<br/>
-<a href="#" target="_blank">Facebook ID</a><br/>
-<br/>
-<a href="#" target="_blank">Linkedin ID</a><br/>
-<br/>
-<br/>
-<b>Back End Developer</b><br/>
-<br/>
-Vinayak Sachdeva<br/>
-<br/>
-College - MNIT Jaipur<br/>
-<br/>
-Email ID - vishusachdeva228@gmail.com<br/>
-<br/>
-<a href="https://www.facebook.com/vinayak.sachdeva.3" target="_blank">Facebook ID</a><br/>
-<br/>
-<a href="https://www.linkedin.com/in/vinayak-sachdeva-415103105/" target="_blank">Linkedin ID</a>
-<br/>
+<!--<br/><br/><br/><br/>-->
+<!--<b>Sophomores.in<br/>-->
+<!--Project_2<br/>-->
+<!--DilSeDeal</b><br/>-->
+<!--<br/>-->~
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Rubik+Mono+One');
+    .devtitle{
+        font-family: 'Rubik Mono One', sans-serif;
+        font-size : 2.5em;
+    }
+    .frontend{
+        height :100%;
+        width : 50%;
+        position  : fixed;
+        background-color : rgb(37,208,221);
+        opacity : 0.2;
+    }
+    .backend{
+        height : 100%;
+        opacity : 0.2;
+        width : 50%;
+        position : fixed;
+        left : 50%;
+        background-color : rgb(94,119,136);
+    }
+    body{
+        background-image:url("../img/backaboutus.jpg");
+        width : 50%;
+    }
+    .frontend-text{
+        width : 50%;
+        color : #fff;
+        position : fixed;
+        top : 80px;
+        text-align:center;
+    }
+    .backend-text{
+        width : 50%;
+        top : 80px;
+        color : #fff;
+        position : fixed;
+        left : 50%;
+        text-align : center;
+    }
+    #gitic1{
+        position : relative;
+        left : -10%;
+        top: 100px;
+    }
+    #gitic2{
+        position : relative;
+        left : -10%;
+        top : 100px;
+    }
+    #fbic1{
+        position  :relative;
+        top : 100px;
+    }
+    #fbic2{
+        position  :relative;
+        top : 100px;
+    }
+    #linkic1{
+        position : relative;
+        top : 100px;
+        left : 10%;
+    }
+    
+    #linkic2{
+        position : relative;
+        top : 100px;
+        left : 10%;
+    }
+    
+</style>
+<body>
+<div class = "frontend"></div>
+<div class  ="frontend-text">
+    <b><div class = "devtitle">FRONT-END DEVELOPER</div></b><br><br>
+    <span style="font-size : 2.5em;">Salil Jain</span><br>
+    <span style = "font-size : 1.8em;top :10px; position : relative">BITS Goa</span><br>
+    <span style = "top:20px; position:relative"><b>j.salil36@gmail.com</b></span><br/>
+    <br/>
+    <a href="https://github.com/Salil-sopho/" target="_blank"><img id = "gitic1" src = "../img/giticon.png" style = "width : 40px;"></a>
+    <a href="https://www.facebook.com/salil.jain.5891" target="_blank"><img id = "fbic1" src = "../img/fbicon.png" style = "width : 40px;"></a>
+    <a href="https://www.linkedin.com/in/salil-jain-328701135/" target="_blank"><img id = "linkic1" src = "../img/linkedinicon.png" style = "width : 40px;"></a>
+</div>
+<div class = "backend"></div>
+<div class = "backend-text">    
+    <b><div class = "devtitle">BACK-END DEVELOPER</div></b><br><br>
+    <span style="font-size : 2.5em;">Vinayak Sachdeva</span><br>
+    <span style = "font-size : 1.8em;top :10px; position : relative">MNIT Jaipur</span><br>
+    <span style = "top:20px; position:relative"><b>vishusachdeva228@gmail.com</b></span><br/>
+    <br/>
+    <a href="https://github.com/Vinayak-sophos" target="_blank"><img id = "gitic2" src = "../img/giticon.png" style = "width : 40px;"></a>
+    <a href="https://www.facebook.com/vinayak.sachdeva.3" target="_blank"><img id = "fbic2" src = "../img/fbicon.png" style = "width : 40px;"></a>
+    <a href="https://www.linkedin.com/in/vinayak-sachdeva-415103105/" target="_blank"><img id = "linkic2" src = "../img/linkedinicon.png" style = "width : 40px;"></a>
+</div>    
+</body>
+    
